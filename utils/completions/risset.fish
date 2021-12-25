52e8e6925889d2964bcdd5e9b2ef046f87be38e8
gesellkammer <eduardo.moguillansky@gmail.com>
2021-12-25 23:09:02 +0100

    add risset plugin to index

--- utils/completions/risset.fish
+++ utils/completions/risset.fish
@@ -1,5 +1,5 @@
 # fish completions for risset
-set -l commands list install remove show man update list-opcodes
+set -l commands list install remove show man update listopcodes
 
 complete -f -c risset -l update -d 'Update rissets database before running any command'
 complete -f -c risset -l debug -d 'Run command showing debugging information'
@@ -11,10 +11,12 @@ complete -f -c risset -n "not __fish_seen_subcommand_from $commands" -a show -d 
 complete -f -c risset -n "not __fish_seen_subcommand_from $commands" -a man -d 'Open manual page for an opcode'
 complete -f -c risset -n "not __fish_seen_subcommand_from $commands" -a update -d 'Update rissets data repository'
 complete -f -c risset -n "not __fish_seen_subcommand_from $commands" -a remove -d 'Remove a package'
-complete -f -c risset -n "not __fish_seen_subcommand_from $commands" -a list-opcodes -d 'List installed opcodes'
+complete -f -c risset -n "not __fish_seen_subcommand_from $commands" -a listopcodes -d 'List installed opcodes'
+complete -f -c risset -n "not __fish_seen_subcommand_from $commands" -a resetcache -d 'Remove local clones'
 
 
-complete -f -c risset -n "__fish_seen_subcommand_from man" -a "(risset list-opcodes)"
+
+complete -f -c risset -n "__fish_seen_subcommand_from man" -a "(risset listopcodes)"
 complete -f -c risset -n "__fish_seen_subcommand_from install" -a "(risset list --notinstalled --nameonly)"
 complete -f -c risset -n "__fish_seen_subcommand_from remove" -a "(risset list --installed --nameonly)"
 complete -f -c risset -n "__fish_seen_subcommand_from show" -a "(risset list --nameonly)"
